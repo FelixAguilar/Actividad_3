9b9716933bb94ff1520b1870969a87fe6118898d
Adrián Bennasar Polzin <49955569+Krymsone@users.noreply.github.com>
2019-10-08 16:51:11 +0200

    Add files via upload

--- Actividad 3/src/activitat3/Lista.java
+++ Actividad 3/src/activitat3/Lista.java
@@ -17,6 +17,42 @@ public class Lista {
         primer = null;
     }
     
+    public void ejercicio2Apartado1(Nodo element){
+            Nodo aux;
+            aux = primer;
+            while(aux.getSiguiente() != element){
+                aux = aux.getSiguiente();
+            }
+            aux.setSiguiente(aux.getSiguiente().getSiguiente());
+        }
+    
+    public Nodo ejercicio2Apartado2(int info){
+        Nodo elemento;
+        elemento = primer;
+        while(elemento.getInfo() != info){
+            elemento = elemento.getSiguiente();
+        }
+        return elemento;
+    }
+    
+    public void ejercicio3(int k) throws ExcepcionListaVacia {
+        int n = 0;
+        Nodo aux = primer;
+        // si la lista esta vacía
+        if (primer == null) {
+            throw new ExcepcionListaVacia();
+        } else if (primer.getSiguiente() == null) {
+            primer = null;
+        } else {
+            while (n < k - 2) {
+                primer = primer.getSiguiente();
+                n++;
+            }
+                primer.setSiguiente(primer.getSiguiente().getSiguiente());
+            }
+
+        }
+    
     public void Ejercicio5 (Nodo nuevo){
         if (nuevo.getInfo()>primer.getInfo() && primer != null) {
             Nodo A = primer;
@@ -31,6 +67,8 @@ public class Lista {
             primer.setSiguiente(nuevo);
         }
     }
+    
+    
     
     public String ToString(){
         String s = "";
